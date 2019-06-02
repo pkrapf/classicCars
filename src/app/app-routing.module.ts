518057fb527e2da12b61bac3a80f0527e68c0c64
Paul Krapf <paulkrapf@gmail.com>
2019-06-02 14:57:09 -0500

    classicCars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CarsForSaleComponent } from './cars-for-sale/cars-for-sale.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CarsAdminComponent } from './cars-admin/cars-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: AboutUsComponent},
+{path: 'about-us', component: AboutUsComponent},
+{path: 'cars-for-sale', component: CarsForSaleComponent},
+{path: 'contact-us', component: ContactUsComponent},
+{path: 'cars-admin', redirectTo: ''},
+{path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CarsForSaleComponent } from './cars-for-sale/cars-for-sale.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CarsAdminComponent } from './cars-admin/cars-admin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    CarsForSaleComponent,
+    ContactUsComponent,
+    CarsAdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
